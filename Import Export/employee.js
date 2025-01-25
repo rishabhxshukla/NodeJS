2f40f8eba409d0a0a2ce215740daaca7269589c3
Rishabh Shukla <rs515666@gmail.com>
2025-01-25 12:57:12 +0530

    Added new files

---
+++ Import Export/employee.js
@@ -0,0 +1,8 @@
+const employee = {
+    name: "Rishabh",
+    age: 22,
+    salary: 70000
+}
+
+// module.exports = employee;
+export default employee;
---
+++ Import Export/index.js
@@ -0,0 +1,6 @@
+// const employee = require("./employee.js");
+import employee from "./employee.js";
+
+console.log("Name : " + employee.name);
+console.log("Age : " + employee.age);
+console.log("Salary : " + employee.salary);
